6a03e155fe3cfec0c2b5f8fdd316c0b835eb4c6b
Abhinav Sachdeva <happy257@gmail.com>
2017-02-06 14:36:24 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,72 @@
 //Put custom code here
+
+// GLOBAL VARIABLES
+var GLOBAL={}
+GLOBAL.empContLimit = 0;
+
+GLOBAL.contLimitInd = 3400;
+GLOBAL.contLimitNotInd = 6750;
+GLOBAL.contLimitIndOver55 = 4400;
+GLOBAL.contLimitNotIndOver55 = 7750;
+
+GLOBAL.getCovTier = function(){
+	return $("#covTier").val();
+}
+GLOBAL.getAge = function(){
+	return $("#inpage").val();
+}
+
+//Returns only integer of the field passed as param or zero
+GLOBAL.getVal = function(field){
+	if(!(field))
+		console.error('"Field" not passed to getVal');
+	
+	var fieldValue=$("#"+field).val()
+	if(isNaN(fieldValue)||fieldValue=="")
+		return 0;
+	else
+		return parseInt(fieldValue);
+}
+
+var goBack = function() {
+    window.history.back()
+}
+
+var updateContLimit = function(){
+  //Needed to initialize slider before refresh
+  $( "#annualCont" ).slider();
+  
+  var age = GLOBAL.getAge();
+  var covTier = GLOBAL.getCovTier();
+  
+	if(age<=55){
+		if(covTier==1){
+			GLOBAL.empContLimit = GLOBAL.contLimitInd;
+		}else{
+			GLOBAL.empContLimit = GLOBAL.contLimitNotInd;	
+		}
+	}else{
+		if(covTier==1){
+			GLOBAL.empContLimit = GLOBAL.contLimitIndOver55;
+		}else{
+			GLOBAL.empContLimit = GLOBAL.contLimitNotIndOver55;
+		}
+	}
+	
+  //Reset slider limits  
+	$("#annualCont").prop({
+	  min:0,
+	  max:GLOBAL.empContLimit
+	}).slider("refresh");
+}
+$('#covTier').on("change",function () {
+	updateContLimit();
+});
+$('#inpage').on("change",function () {
+	console.log('Age altered');
+	updateContLimit();
+});
+
 var buildTaxPopup = function(){
 	var taxSet=[];
 	taxSet[1] ={filingStat:"Single",taxableIncome:['1A','2','3','4','5','6','7']};
