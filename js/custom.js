0ab05f91163ccefa4a93a380728d0c3de26577e9
Abhinav Sachdeva <happy257@gmail.com>
2017-02-11 09:33:02 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 //Put custom code here
 
 // GLOBAL VARIABLES
-var GLOBAL={}
+var GLOBAL={};
 GLOBAL.empContLimit = 0;
 
 GLOBAL.contLimitInd = 3400;
@@ -12,9 +12,12 @@ GLOBAL.contLimitNotIndOver55 = 7750;
 GLOBAL.getCovTier = function(){
 	return $("#covTier").val();
 }
-GLOBAL.getAge = function(){
-	return $("#inpage").val();
+GLOBAL.getAge = function(){	
+	if(!$("#inpage").val()){$("#inpage").val('25')};	
+	return parseInt($("#inpage").val());
 }
+
+
 
 //Returns only integer of the field passed as param or zero
 GLOBAL.getVal = function(field){
@@ -23,14 +26,22 @@ GLOBAL.getVal = function(field){
 	
 	var fieldValue=$("#"+field).val()
 	if(isNaN(fieldValue)||fieldValue=="")
-		return 0;
-	else
+		$("#"+field).val('0');	
 		return parseInt(fieldValue);
 }
-
+var setMTRate = function(selectedRateElem){
+	var selRate=selectedRateElem.getAttribute("value");
+	console.log(selRate);
+	$($("#taxRate option").get(selRate)).attr('selected', 'selected')
+	$("#taxRate").selectmenu();
+        $("#taxRate").selectmenu('refresh', true);
+	//$("#taxRate option[value='test1']").attr("selected", "selected");
+	closePop('#taxCalcPopup')
+	}
 var goBack = function() {
     window.history.back()
 }
+var closePop = function(popupDiv){$(popupDiv).popup( "close" );}	
 
 var updateContLimit = function(){
   //Needed to initialize slider before refresh
@@ -51,21 +62,56 @@ var updateContLimit = function(){
 		}else{
 			GLOBAL.empContLimit = GLOBAL.contLimitNotIndOver55;
 		}
-	}
-	
+	}	
   //Reset slider limits  
 	$("#annualCont").prop({
 	  min:0,
 	  max:GLOBAL.empContLimit
 	}).slider("refresh");
+	$.mobile.navigate( "#flow2" );
 }
-$('#covTier').on("change",function () {
+var updateWithdrawlLimit = function(){
+  //Needed to initialize slider before refresh
+  $( "#annualWithdrawl" ).slider();
+  
+  var maxWithdrawl = GLOBAL.getVal('currHSABal')+GLOBAL.getVal('annualCont');
+  
+  //Reset slider limits  
+	
+	if(maxWithdrawl==0){
+		console.log("0000000");
+		updateAccountGrowthYears()
+		}else{
+				$("#annualWithdrawl").prop({
+	  			min:0,
+	  			max:maxWithdrawl
+				}).slider("refresh");
+				$.mobile.navigate( "#flow3" );
+			}
+}
+var updateAccountGrowthYears = function(){
+  //Needed to initialize slider before refresh
+ $.mobile.navigate( "#flow4" );
+  //$( "#years" ).slider();
+ 
+  //Reset slider limits  
+$( '#flow4' ).on( 'pageinit',function(event){
+	var maxYears = 100-GLOBAL.getAge();
+  $("#years").prop({
+	  min:0,
+	  max:maxYears
+	}).slider("refresh");
+});  
+		
+}
+
+/*$('#covTier').on("change",function () {
 	updateContLimit();
 });
 $('#inpage').on("change",function () {
 	console.log('Age altered');
 	updateContLimit();
-});
+});*/
 
 var buildTaxPopup = function(){
 	var taxSet=[];
@@ -80,11 +126,14 @@ var buildTaxPopup = function(){
 		container += '<div data-role="collapsible"><h3>'+taxSet[i].filingStat+'</h3>';
 		var lists='<ul data-role="listview">'
 		for(j=0;j<taxSet[i].taxableIncome.length;j++){
-			lists+='<li><a href="#" onClick="">'+taxSet[i].taxableIncome[j]+'</a></li>'
+			lists+='<li><a href="#" value='+(j+1)+' onClick="setMTRate(this)">'+taxSet[i].taxableIncome[j]+'</a></li>'
 		}
 		lists+='</ul>'
 		container+= lists+'</div>'
 	}
-	$("#taxSet").html(container);
+	$("#taxSet").html(container);	
 }
 buildTaxPopup();
+
+
+setTimeout(function(){new Chartist.Line('#chart1', { labels: [1, 2, 3, 4], series: [[100, 120, 180, 200]] })},1000); 
